b81b30422f68c55cb642076aa160dba012dd9d80
Robert-Lexicon <robert.hedblad@lexicon.se>
2026-02-23 13:48:07 +0100

    Examples on how to update, delete and edit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { DeleteForm } from "@/components/delete-form";
 import type { ProductsResponse } from "./types";
 
 const API_URL = "http://localhost:4000";
@@ -12,13 +14,27 @@ export default async function Home() {
     `${API_URL}/products/?_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category`,
   ).then((res) => res.json());
 
-
-console.log(products);
+  console.log(products);
 
   return (
-    <main>
-      <h1>Products</h1>
-      <div>{products.map((product) => <h2 key={product.id}>{product.title} - {product.category?.name}</h2>)}</div>
+    <main className="grid">
+      <header className="flex justify-between">
+        <h1>Products</h1>
+        <Link className="bg-neutral-200 rounded p-4" href="/products/create">
+          Add Product
+        </Link>
+      </header>
+      <div>
+        {products.map((product) => (
+          <div key={product.id} className="border-b flex gap-4">
+            <h2>
+              {product.title} - {product.category?.name}
+            </h2>
+            <Link href={`/products/edit/${product.id}`}>Edit</Link>
+            <DeleteForm id={product.id.toString()} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
